20cc67dbb8d93b9fbaeb3b6d4078cb837c445a10
Andrew Silaghi <silaghiandrew89@gmail.com>
2019-08-22 22:02:28 -0500

    third

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,9 +3,11 @@ import { Link } from "gatsby"
 import HamburgerMenu from "../components/hamburger-menu"
 
 class NavBar extends Component {
+
   componentDidMount() {
     window.addEventListener("scroll", this.toggleBodyClass);
     this.toggleBodyClass();
+ 
   }
 
   componentWillUnmount() {
@@ -14,16 +16,16 @@ class NavBar extends Component {
 
   toggleBodyClass = () => {
     if (window.scrollY < 100) {
-      document.nav.classList.add("bg-black");
+      document.getElementById('nav').classList.remove("bg-black");
     } else {
-      document.nav.classList.remove("bg-black");
+      document.getElementById('nav').classList.add("bg-black");
     }
-  };
+  }
   render(){    
     
     return (
       <div>
-  <nav className="navbar navbar-expand navbar-dark flex-column flex-md-row fixed-top" >
+  <nav id="nav" className="navbar navbar-expand navbar-dark flex-column flex-md-row fixed-top why-u-no-work" >
 <div className="container py-3" style={{ color: `black` }}>
   <Link className="mx-auto d-block" to="/"><img className="my-auto" src="http://andrewsilaghi.com/ox/wp-content/uploads/2019/08/ox-logo-lg.png" alt="Logo" width="50px"/></Link>
   </div>
